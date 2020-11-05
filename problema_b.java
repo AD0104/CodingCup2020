867ad57d68ce4243c707ce55c6313724ad817faf
AD0104 <ad24.ra12@gmail.com>
2020-11-05 16:11:10 -0600

    Resuelto problema 2

--- problema_b.java
+++ problema_b.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class problema_b {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> calificaciones = new ArrayList<>();
+        ArrayList<Integer> promedios = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String entry = br.readLine();
+            String[] entradas = entry.split(" ");
+            calificaciones.add(Integer.parseInt(entradas[0]));
+            promedios.add(Integer.parseInt(entradas[1]));
+        }
+        for (int i = 0; i < calificaciones.size(); i++) {
+            for (int j = 0; j < calificaciones.get(i); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        int suma=0;
+        for (int i = 0; i < calificaciones.size(); i++) {
+            suma+=calificaciones.get(i);
+        }
+        int promedio = suma/n;
+        if(promedio >= 6)
+            System.out.println(promedio+"\n:)");
+        else
+            System.out.println("0\n:(");
     }
 }
