4683bdeaac5d4f9ace49f59e24812b8c8657f515
SevroAuBarca <daniel_contreras001@hotmail.com>
2020-11-06 10:49:43 -0600

    programa E

---
+++ E.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daniel.alejandro.contreras.martinez.concurso;
+
+import java.util.Scanner;
+
+public class E {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x;
+        String color;
+
+        color = s.next();
+        x = s.nextInt();
+        if (x >= 1 && x <= 1000) {
+            if (color.equals("R") || color.equals("A") || color.equals("V")) {
+                switch(color){
+                    case "R":
+                        if(x%2== 1){
+                            color = "A";
+                            System.out.println(color);
+                        }else if(x%2 !=1){
+                            color = "V";
+                            System.out.println(color);
+                        }
+                        break;
+                    case "A":
+                        break;
+                    case "V":
+                        break;
+                }
+                
+                
+            }
+        }
+    }
+
+}
